89c6e0b74fc83bb0c4baafdc258f60d4ae617114
AdrianGerman <adrian.german1019@gmail.com>
2025-08-23 08:41:04 -0600

    page controller and question model

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $questions = Question::with('category', 'user')->latest()->get();
+
+        return view('pages.home', [
+            'questions' => $questions,
+        ]);
     }
 }
