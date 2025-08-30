9a09c0fb8d01f2a9051f21f0856e00a70aff375c
AdrianGerman <adrian.german1019@gmail.com>
2025-08-30 09:16:30 -0600

    Comment livewire

--- app/Livewire/Comment.php
+++ app/Livewire/Comment.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire;
 
+use Illuminate\Database\Eloquent\Model;
 use Livewire\Component;
 
 class Comment extends Component
 {
+    public Model $commentable;
+    public bool $showForm = false;
+
+    public function toggle()
+    {
+        $this->showForm = !$this->showForm;
+    }
+
     public function render()
     {
-        return view('livewire.comment');
+        return view('livewire.comment', [
+            'comments' => $this->commentable->comments,
+        ]);
     }
 }
