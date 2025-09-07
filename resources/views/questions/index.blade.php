54c6a550bf4da3cda87d852528c9b233708a2332
AdrianGerman <adrian.german1019@gmail.com>
2025-09-07 08:57:20 -0600

    index blade in questions view

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,26 +1,24 @@
 <x-forum.layouts.app>
     <div class="my-8">
 
-        <!-- foreach / questions -->
+        @foreach ($questions as $question)
+            <div class="mb-4">
+                <h2 class="text-2xl font-bold">
+                    <a href="{{ route('questions.show', $question) }}" class="hover:underline">
+                        {{ $question->title }}
+                    </a>
+                </h2>
 
-        <div class="mb-4">
-            <h2 class="text-2xl font-bold">
-                <a href="#" class="hover:underline">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit
-                </a>
-            </h2>
-
-            <div class="flex gap-2">
-                <p class="text-xs text-gray-500">
-                    <span class="font-semibold">User</span> |
-                    Category |
-                    Created at
-                </p>
+                <div class="flex gap-2">
+                    <p class="text-xs text-gray-500">
+                        <span class="font-semibold">{{ $question->user->name }}</span> |
+                        {{ $question->category->name }} |
+                        {{ $question->created_at->diffForHumans() }}
+                    </p>
+                </div>
             </div>
-        </div>
+        @endforeach
 
-        <!-- endforeach -->
-
-        <!-- Paginate -->
+        {{ $questions->links() }}
     </div>
 </x-forum.layouts.app>
