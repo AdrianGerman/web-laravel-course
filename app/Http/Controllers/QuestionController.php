78b5e52df86af99a3845680fdb52ba151db75664
AdrianGerman <adrian.german1019@gmail.com>
2025-09-12 08:36:06 -0600

    change example user_id 20 for auth

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -39,7 +39,7 @@ class QuestionController extends Controller
         ]);
 
         $question = Question::create([
-            'user_id' => 20,
+            'user_id' => auth()->id(),
             'category_id' => $request->category_id,
             'title' => $request->title,
             'description' => $request->description,
@@ -76,7 +76,7 @@ class QuestionController extends Controller
 
     public function show(Question $question)
     {
-        $userId = 20;
+        $userId = auth()->id();
 
         $question->load([
             'user',
