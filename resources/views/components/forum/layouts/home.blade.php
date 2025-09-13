783880c99f825f3b046289b69a636ee78e039928
AdrianGerman <adrian.german1019@gmail.com>
2025-09-13 09:03:46 -0600

    guest in login option

--- resources/views/components/forum/layouts/home.blade.php
+++ resources/views/components/forum/layouts/home.blade.php
@@ -38,7 +38,10 @@
                     <div class="flex items-center justify-center gap-6">
                         <a href="{{ route('questions.create') }}"
                             class="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500">Preguntar</a>
-                        <a href="#" class="text-sm font-semibold text-gray-900">Login &rarr;</a>
+                        @guest
+                            <a href="{{ route('login') }}" class="text-sm font-semibold text-gray-900">Login
+                                &rarr;</a>
+                        @endguest
                     </div>
                 </div>
             </div>
