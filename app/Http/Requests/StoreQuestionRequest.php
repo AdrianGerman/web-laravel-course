ce2915c0790cc0da2f19eef8c5a5b017cbcf2549
AdrianGerman <adrian.german1019@gmail.com>
2025-09-16 07:26:39 -0600

    create validations in store question

--- app/Http/Requests/StoreQuestionRequest.php
+++ app/Http/Requests/StoreQuestionRequest.php
@@ -11,7 +11,7 @@ class StoreQuestionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreQuestionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'category_id' => 'required|exists:categories,id',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
         ];
     }
 }
