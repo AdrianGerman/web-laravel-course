108a8cc6e7e5e51a81b3a5532fac12feb7c72741
AdrianGerman <adrian.german1019@gmail.com>
2025-09-21 08:35:15 -0600

    confirm password translate

--- resources/views/livewire/auth/confirm-password.blade.php
+++ resources/views/livewire/auth/confirm-password.blade.php
@@ -1,24 +1,14 @@
 <div class="flex flex-col gap-6">
-    <x-auth-header
-        :title="__('Confirm password')"
-        :description="__('This is a secure area of the application. Please confirm your password before continuing.')"
-    />
+    <x-auth-header :title="__('Confirmar contraseña')" :description="__('Esta es un área segura de la aplicación. Por favor confirma tu contraseña antes de continuar.')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <form method="POST" wire:submit="confirmPassword" class="flex flex-col gap-6">
         <!-- Password -->
-        <flux:input
-            wire:model="password"
-            :label="__('Password')"
-            type="password"
-            required
-            autocomplete="new-password"
-            :placeholder="__('Password')"
-            viewable
-        />
+        <flux:input wire:model="password" :label="__('Contraseña')" type="password" required autocomplete="new-password"
+            :placeholder="__('Contraseña')" viewable />
 
-        <flux:button variant="primary" type="submit" class="w-full">{{ __('Confirm') }}</flux:button>
+        <flux:button variant="primary" type="submit" class="w-full">{{ __('Confirmar') }}</flux:button>
     </form>
 </div>
