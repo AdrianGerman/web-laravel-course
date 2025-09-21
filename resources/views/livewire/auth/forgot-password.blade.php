e09cbf8cf5c4c5fa679c482bb986fa8f3b6472db
AdrianGerman <adrian.german1019@gmail.com>
2025-09-21 08:35:48 -0600

    forgot password translate

--- resources/views/livewire/auth/forgot-password.blade.php
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -1,25 +1,20 @@
- <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Forgot password')" :description="__('Enter your email to receive a password reset link')" />
+<div class="flex flex-col gap-6">
+    <x-auth-header :title="__('Olvidé mi contraseña')" :description="__('Ingresa tu correo para recibir un enlace de restablecimiento de contraseña')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <form method="POST" wire:submit="sendPasswordResetLink" class="flex flex-col gap-6">
         <!-- Email Address -->
-        <flux:input
-            wire:model="email"
-            :label="__('Email Address')"
-            type="email"
-            required
-            autofocus
-            placeholder="email@example.com"
-        />
+        <flux:input wire:model="email" :label="__('Correo electrónico')" type="email" required autofocus
+            placeholder="correo@ejemplo.com" />
 
-        <flux:button variant="primary" type="submit" class="w-full">{{ __('Email password reset link') }}</flux:button>
+        <flux:button variant="primary" type="submit" class="w-full">{{ __('Enviar enlace de restablecimiento') }}
+        </flux:button>
     </form>
 
     <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-400">
-        <span>{{ __('Or, return to') }}</span>
-        <flux:link :href="route('login')" wire:navigate>{{ __('log in') }}</flux:link>
+        <span>{{ __('O bien, regresa a') }}</span>
+        <flux:link :href="route('login')" wire:navigate>{{ __('iniciar sesión') }}</flux:link>
     </div>
 </div>
